f44034264e7f331a457402a1681704b123fc29ae
Bodu-glitch <tn212079@gmail.com>
2025-08-22 14:01:28 +0700

    feat:setup following layout

---
+++ src/app/pages/following/components/following-user/following-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-following-user',
+  imports: [],
+  templateUrl: './following-user.component.html',
+  styleUrl: './following-user.component.scss'
+})
+export class FollowingUserComponent {
+
+}
