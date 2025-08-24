4cd5dd584ead960fc4488ae24a95b1b00ff5bb30
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-08-24 22:10:53 +0700

    Feat: Add video cards component to user profile page

--- src/app/pages/profile/components/playlist/playlist.component.ts
+++ src/app/pages/profile/components/playlist/playlist.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {VideoCardComponent} from '../video-card/video-card.component';
 
 @Component({
     selector: 'app-playlist',
-    imports: [],
+  imports: [
+    MatTab,
+    MatTabGroup,
+    VideoCardComponent
+  ],
     templateUrl: './playlist.component.html',
     styleUrl: './playlist.component.scss'
 })
