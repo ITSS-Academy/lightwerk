debbe5a76bef4b2aa6fc375dbb16bf7dec9e812f
Thinh161 <thinh.bdd03685@sinhvien.hoasen.edu.vn>
2025-08-25 16:45:12 +0700

    feat: implement user following and grid layout components

--- src/app/pages/following/components/following-user/following-user.component.ts
+++ src/app/pages/following/components/following-user/following-user.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  MatCard, MatCardAvatar,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+  MatCardTitleGroup
+} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-following-user',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitleGroup,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    MatCardAvatar,
+    MatButton
+  ],
   templateUrl: './following-user.component.html',
   styleUrl: './following-user.component.scss'
+
 })
 export class FollowingUserComponent {
+  followStates = Array(50).fill(true);
 
+  toggleFollow(index: number) {
+    this.followStates[index] = !this.followStates[index];
+  }
 }
