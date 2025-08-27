a3c31cc80f48653bb6e1a970199190d1059cdca6
tuan2304 <tuan.vha03851@sinhvien.hoasen.edu.vn>
2025-08-27 09:29:24 +0700

    fix: M3 create-video and upload-detail

--- src/app/pages/exploring/exploring.component.ts
+++ src/app/pages/exploring/exploring.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import {MatChipListbox, MatChipOption} from '@angular/material/chips';
-import {MatCard, MatCardContent, MatCardImage} from '@angular/material/card';
 
 @Component({
   selector: 'app-exploring',
   imports: [
-    MatChipListbox,
-    MatChipOption,
-    MatCard,
-    MatCardImage,
-    MatCardContent
   ],
   templateUrl: './exploring.component.html',
   styleUrl: './exploring.component.scss'
